5e64ee86e4a002ba1a267d46aea1ebca89d51a43
paulacruz <cruz.amp12@gmail.com>
2019-09-19 21:17:48 -0700

    trying to push new files

---
+++ code401challenges/src/test/java/code401codechallenges/testBinarySearch.java
@@ -0,0 +1,17 @@
+package code401codechallenges;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testBinarySearch {
+  @Test
+  public void testBinarySearch()
+  {
+    int[] newArray = new int[]{4, 8, 15, 16, 23, 42};
+    int expected = 2;
+    assertEquals("resulting binary should be 2 or -1",
+            expected,
+            BinarySearch.binarySearch(newArray, 15));
+  };
+}
+
